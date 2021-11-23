5ccf73030be2814287c4a1136bd8a300d3dce3b0
Diego Cattarinich Clavel <dev.cattarinich@gmail.com>
2021-11-23 08:19:53 -0300

    fixed methods and finished leia

---
+++ cmd/leia/service/droid_data.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	"sync"
+
+	"github.com/delCatta/rebels/pb"
+)
+
+type DroidData struct {
+	mutex       *sync.RWMutex
+	registers   []*RV
+	lastAddress map[string]*pb.FulcrumAddress
+}
+
+func NewDroidData() *DroidData {
+	return &DroidData{
+		mutex:       &sync.RWMutex{},
+		registers:   []*RV{},
+		lastAddress: make(map[string]*pb.FulcrumAddress),
+	}
+}
+
+type RV struct {
+	req    *pb.LeiaReq
+	vector *pb.VectorClock
+}
+
+func (data *DroidData) Save(req *pb.LeiaReq, res *pb.BrokerAmountRes) {
+	rv := &RV{req: req, vector: res.GetVector()}
+
+	data.mutex.Lock()
+	defer data.mutex.Unlock()
+
+	data.registers = append(data.registers, rv)
+	data.lastAddress[rv.identifier()] = res.GetAddress()
+	// TODO: Notify Saved Data!
+}
+
+func (rv *RV) identifier() string {
+	return rv.req.NombrePlaneta + "-" + rv.req.NombreCiudad
+}
