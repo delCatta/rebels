5ac3a43935123fa570e0736d98d8505b151155ea
ftcister <felipe.cisternas.alvarez@hotmail.com>
2021-12-06 22:26:30 -0300

    broker PickAClient y HowManyRebels

--- cmd/broker/service/server.go
+++ cmd/broker/service/server.go
@@ -3,6 +3,8 @@ package service
 import (
 	"context"
 	"fmt"
+	"math/rand"
+	"time"
 
 	"github.com/delCatta/rebels/pb"
 	"google.golang.org/grpc"
@@ -37,6 +39,9 @@ func NewFulcrumClient(address string, server *BrokerServer) *pb.LightSpeedCommsC
 
 }
 
+// Deberia ser eso, pero en el pdf dice:
+// TODO: Redirige a los Informantes a una réplica en específico cuando estas tengan un conflicto con las versiones de los Registros Planetarios.
+// y no entiendo a que se refiere...
 func (server *BrokerServer) InformarBroker(ctx context.Context, req *pb.InformanteReq) (*pb.BrokerRes, error) {
 	fulcrumClient := server.pickAClient()
 	address := server.addresses[fulcrumClient]
@@ -45,39 +50,57 @@ func (server *BrokerServer) InformarBroker(ctx context.Context, req *pb.Informan
 	}
 	return response, nil
 
-	// Deberia ser eso, pero en el pdf dice:
-	// TODO: Redirige a los Informantes a una réplica en específico cuando estas tengan un conflicto con las versiones de los Registros Planetarios.
-	// y no entiendo a que se refiere...
 }
 
+// TODO: Redirige a la Princesa Leia a una réplica en específico cuando estas tengan un conflicto con las versiones de los Registros Planetarios.
+// No entiendo a lo que se refiere...
+
+// TODO: Acá hay que hacer una request al Fulcrum:
+// request = *pb.AlgunaRequest{}
+// res,err := (*fulcrumClient).Metodo(request)
+// if err!=nil{
+// 	return nil, err
+// }
 func (server *BrokerServer) HowManyRebelsBroker(ctx context.Context, req *pb.LeiaReq) (*pb.BrokerAmountRes, error) {
-
-	// TODO: Redirige a la Princesa Leia a una réplica en específico cuando estas tengan un conflicto con las versiones de los Registros Planetarios.
-	// No entiendo a lo que se refiere...
-
 	fulcrumClient := server.pickAClient()
-
-	// TODO: Acá hay que hacer una request al Fulcrum:
-	// request = *pb.AlgunaRequest{}
-	// res,err := (*fulcrumClient).Metodo(request)
-	// if err!=nil{
-	// 	return nil, err
-	// }
+	request := &pb.LeiaReq{}
+	res, err := (*fulcrumClient).HowManyRebelsBroker(ctx, request)
+	if err != nil {
+		fmt.Printf("Error calling HowManyRebels: %e\n", err)
+		return nil, err
+	}
 
 	// TODO: Y luego devolverle la info a la Princesa leia
 	response := &pb.BrokerAmountRes{
 		Address: server.addresses[fulcrumClient],
-		Vector:  nil, // res.GetVector()
-		Amount:  0,   // res.GetAmount()
+		Vector:  res.GetVector(), // res.GetVector()
+		Amount:  res.GetAmount(), // res.GetAmount()
 	}
 	return response, nil
 }
 
+// TODO: Hacer esta funcion que elija un cliente random entre:
+// server.fulcrum1
+// server.fulcrum2
+// server.fulcrum3
+// Revisar que no sea nula la conexión (ejemplo server.fulcrum1 == nil)
 func (server *BrokerServer) pickAClient() *pb.LightSpeedCommsClient {
-	// TODO: Hacer esta funcion que elija un cliente random entre:
-	// server.fulcrum1
-	// server.fulcrum2
-	// server.fulcrum3
-	// Revisar que no sea nula la conexión (ejemplo server.fulcrum1 == nil)
-	return server.fulcrum1
+	rand.Seed(time.Now().UnixNano())
+	random_int := rand.Intn(3)
+	if random_int == 0 {
+		if server.fulcrum1 != nil {
+			return server.fulcrum1
+		}
+	}
+	if random_int == 1 {
+		if server.fulcrum2 != nil {
+			return server.fulcrum2
+		}
+	}
+	if random_int == 2 {
+		if server.fulcrum3 != nil {
+			return server.fulcrum3
+		}
+	}
+	return nil
 }
