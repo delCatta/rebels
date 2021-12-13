67596ba31b7e5e3c127821e9618b3830f3f02135
Lucas Galindo <lanternmaze@gmail.com>
2021-12-13 11:35:14 -0300

    actualizado main.go del fulcrum

--- cmd/fulcrum/main.go
+++ cmd/fulcrum/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+    // "sync"
 
 	"github.com/delCatta/rebels/cmd/fulcrum/service"
 	"github.com/delCatta/rebels/pb"
@@ -10,12 +11,26 @@ import (
 )
 
 func main() {
-	lis, err := net.Listen("tcp", "localhost:3005") // Puerto 3005!
+	lis, err := net.Listen("tcp4", "localhost:3005") // Puerto 3005!
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	fulcrumServer := service.NewFulcrumServer() // TODO: Implement this FulcrumServer
+	fulcrumServer := service.NewFulcrumServer() 
 	pb.RegisterLightSpeedCommsServer(grpcServer, fulcrumServer)
+
+    // 2 de los fulcrum necesitan esta linea
+    pb.RegisterPropagacionCambiosServer(grpcServer, fulcrumServer)
+
+    // el tercero necesita esto:
+    // wg := new(sync.WaitGroup)
+    // wg.Add(1)
+
+    // go func() {
+    //     fulcrumServer.propagarCambios()
+    //     wg.Done()
+    // }()
 	grpcServer.Serve(lis)
+
+    // wg.Wait()
 }
